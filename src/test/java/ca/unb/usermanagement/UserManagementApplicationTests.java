3ca3bafeff668be1425c9643debb87e4cc153832
Daniel <68374503+biotixin@users.noreply.github.com>
2022-02-25 10:19:45 -0400

    Spring initializer added

---
+++ src/test/java/ca/unb/usermanagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.unb.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
