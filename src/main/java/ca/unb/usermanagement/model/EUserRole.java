0de7bb09e20210346191be1cfe5c2db3de6bcd75
Amanjot Singh <amansinghs7010@gmail.com>
2022-03-08 12:17:55 -0400

    add dependencies

---
+++ src/main/java/ca/unb/usermanagement/model/EUserRole.java
@@ -0,0 +1,7 @@
+package ca.unb.usermanagement.model;
+
+public enum EUserRole {
+	ROLE_USER,
+	ROLE_MODERATOR,
+	ROLE_ADMIN
+}
---
+++ src/main/java/ca/unb/usermanagement/model/UserRole.java
@@ -0,0 +1,34 @@
+package ca.unb.usermanagement.model;
+import javax.persistence.*;
+
+@Entity
+@Table(name="roles")
+public class UserRole {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(length = 20)
+	private EUserRole name;
+	
+	public UserRole() {}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public EUserRole getName() {
+		return name;
+	}
+
+	public void setName(EUserRole name) {
+		this.name = name;
+	}
+	
+}
