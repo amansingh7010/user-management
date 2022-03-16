e95f35e6a2db6e677da138223fe43cc3493987f2
Daniel <68374503+biotixin@users.noreply.github.com>
2022-03-16 14:18:21 -0300

    fixed XLSX issues

--- src/main/java/ca/unb/usermanagement/security/WebSecurityConfig.java
+++ src/main/java/ca/unb/usermanagement/security/WebSecurityConfig.java
@@ -1,4 +1,8 @@
 package ca.unb.usermanagement.security;
+
+import ca.unb.usermanagement.security.jwt.AuthEntryPointJwt;
+import ca.unb.usermanagement.security.jwt.AuthTokenFilter;
+import ca.unb.usermanagement.security.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -14,55 +18,53 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import ca.unb.usermanagement.security.jwt.AuthEntryPointJwt;
-import ca.unb.usermanagement.security.jwt.AuthTokenFilter;
-import ca.unb.usermanagement.security.services.UserDetailsServiceImpl;
-
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig extends  WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Value("${spring.h2.console.path}")
-	private String h2ConsolePath;
-	
-	@Autowired
-	UserDetailsServiceImpl userDetailsService;
-	
-	@Autowired
-	private AuthEntryPointJwt unauthorizedHandler;
-	
-	@Bean
-	public AuthTokenFilter authenticationJwtTokenFilter() {
-		return new AuthTokenFilter();
-	}
-	
-	@Override
-	public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-	  authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-	}
-	
-	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-	  return super.authenticationManagerBean();
-	}
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-	  return new BCryptPasswordEncoder();
-	}
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-	  http.cors().and().csrf().disable()
-	    .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-	    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-	    .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-	    .antMatchers("/api/test/**").permitAll()
-	    .antMatchers(h2ConsolePath + "/**").permitAll()
-	    .anyRequest().authenticated();
-	    
-	  http.headers().frameOptions().sameOrigin();
-	  http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-	}
+    @Value("${spring.h2.console.path}")
+    private String h2ConsolePath;
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests().antMatchers("/api/auth/**").permitAll()
+                .antMatchers("/api/test/**").permitAll()
+                .antMatchers(h2ConsolePath + "/**").permitAll()
+                .antMatchers("/api/report/**").permitAll()
+                .anyRequest().authenticated();
+
+        http.headers().frameOptions().sameOrigin();
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
 }
