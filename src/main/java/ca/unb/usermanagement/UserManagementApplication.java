ddf8e490ef72bbb66abd0efaabd073d736bfbbbd
Daniel <68374503+biotixin@users.noreply.github.com>
2022-03-19 13:43:28 -0300

    auto init roles database

--- src/main/java/ca/unb/usermanagement/UserManagementApplication.java
+++ src/main/java/ca/unb/usermanagement/UserManagementApplication.java
@@ -1,13 +1,35 @@
 package ca.unb.usermanagement;
 
+import ca.unb.usermanagement.model.EUserRole;
+import ca.unb.usermanagement.model.UserRole;
+import ca.unb.usermanagement.repository.UserRoleRepository;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UserManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserManagementApplication.class, args);
-	}
+    private final UserRoleRepository userRoleRepository;
+
+    public UserManagementApplication(UserRoleRepository userRoleRepository) {
+        this.userRoleRepository = userRoleRepository;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagementApplication.class, args);
+    }
+
+    @Bean
+    InitializingBean sendDatabase() {
+        return () -> {
+            for (EUserRole role : EUserRole.values()) {
+                if (!userRoleRepository.findByName(role).isPresent()) {
+                    userRoleRepository.save(new UserRole(role));
+                }
+            }
+        };
+    }
 
 }
